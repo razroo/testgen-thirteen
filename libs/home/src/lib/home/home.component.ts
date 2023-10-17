f5a325c320995cbc6f744564c3b84c38929ecea8
Charlie Greenman <charlie@razroo.com>
2023-10-16 22:47:15 -0400

    Add code for angular-16.2.0 add-homepage-component.
    [Url: razroo.com/community/angular-16.2.0/add-homepage/add-homepage-component]

---
+++ libs/home/src/lib/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'testgen-thirteen-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {}
+
+}
